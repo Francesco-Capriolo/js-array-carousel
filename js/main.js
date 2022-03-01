f6f118878fa83071486a522f6ff18479f3d897eb
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-03-01 16:42:22 +0100

    modifica del bottone next

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-const carouselWrapper = document.querySelector('div.carousel');
-carouselWrapper.innerHTML += carouselContent;
-// § fine aggiunta -->
-
 // Recupero la lista degli elementi del carosello
 const carouselElements = document.getElementsByClassName('carousel-element');
+const carouselImgLeft = document.getElementsByClassName('carousel-left');
 
 // Recupero la lista dei bookmarks (pallini) del carosello
 const bookmarkElements = document.getElementsByClassName('circle');
 
 // Aggiungo al primo elemento la classe active per fare si che sia mostrato
-carouselElements[0].classList.add('active');
 bookmarkElements[0].classList.add('active');
+
+//aggiungo il bordo
+carouselImgLeft[0].classList.add("img-thumbnail");
 console.log(carouselElements);
 
 // Recupero il bottone
@@ -20,18 +19,59 @@ const nextButton = document.querySelector('div.next');
 let activeElement = 0;
 
 nextButton.addEventListener('click', function () {
-    //  remove all'immagini corrente la classe active per renderla invisibile
-    carouselElements[activeElement].classList.remove('active');
+    //  remove all'immagini corrente la classe d-block 
+    carouselElements[activeElement].classList.remove('d-block');
+
+    //aggiungo la classe d-none per renderla invisibile
+    carouselElements[activeElement].classList.add('d-none');
 
     //  remove al bookmark corrente la classe active per renderla invisibile
     bookmarkElements[activeElement].classList.remove('active');
 
+    //rimuovo il bordo
+    carouselImgLeft[activeElement].classList.remove("img-thumbnail");
+
+    //aggiungo l'opacity
+    carouselImgLeft[activeElement].classList.add("opacity-50");
+
     // incrementiamo di uno l'indice dell'immagine selezionata
     activeElement++; // activeElement = activeElement + 1 ;
 
+    //rimuovo la classe d-none
+    carouselElements[activeElement].classList.remove('d-none');
+
     // aggiungere la classe active all'immagine successiva
-    carouselElements[activeElement].classList.add('active');
+    carouselElements[activeElement].classList.add('d-block');
 
     // aggiungere la classe active al bookmark successivo
     bookmarkElements[activeElement].classList.add('active');
+
+    //rimuovo il bordo
+    carouselImgLeft[activeElement].classList.add("img-thumbnail");
+
+    //aggiungo l'opacity
+    carouselImgLeft[activeElement].classList.remove("opacity-50");
 });
+
+const previusButton = document.querySelector('div.previus');
+
+let previusElement = 1;
+
+previusButton.addEventListener('click', function () {
+    //  remove all'immagini corrente la classe active per renderla invisibile
+    carouselElements[previusElement].classList.remove('d-block');
+    carouselElements[previusElement].classList.add('d-none');
+    //  remove al bookmark corrente la classe active per renderla invisibile
+    bookmarkElements[previusElement].classList.remove('active');
+
+    // incrementiamo di uno l'indice dell'immagine selezionata
+    previusElement--; // previusElement = previusElement + 1 ;
+
+    // aggiungere la classe active all'immagine successiva
+    carouselElements[previusElement].classList.add('d-block');
+
+    // aggiungere la classe active al bookmark successivo
+    bookmarkElements[previusElement].classList.add('active');
+});
+
+/*img-thumbnail  */
